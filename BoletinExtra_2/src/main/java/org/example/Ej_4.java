de7f21e67c45bbb7685ea7e37365230ce11bba1d
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-10-10 14:35:00 +0200

    Ejerccio 4 admitiendo decimales y devolviendo tambien las monedas de centimos

--- BoletinExtra_2/src/main/java/org/example/Ej_4.java
+++ BoletinExtra_2/src/main/java/org/example/Ej_4.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Ej_4 {
     public static void main(String []args){
         int B100,B20,B5,M1;
+        int M05,M02,M01,M005,M002,M001;
         double dineroTotal;
         Scanner sc = new Scanner(System.in);
         System.out.println("Cuantos dinero tienes?");
@@ -16,8 +17,21 @@ public class Ej_4 {
         B5 = (int)(dineroTotal/5);
         dineroTotal=dineroTotal-B5*5;
         M1=(int)dineroTotal;
-        dineroTotal = (dineroTotal-M1);
-        System.out.print("Tienes "+B100+" billetes de 100€,"+B20+" billetes de 20€,"+B5+" billetes de 5€, "+M1+" monedas de 1€ y ");
-        System.out.printf("%.2f centimos",dineroTotal);
+        dineroTotal = (dineroTotal-M1)*100+0.0001;
+        System.out.println(dineroTotal);
+        M05 = (int)dineroTotal/50;
+        dineroTotal= dineroTotal-M05*50;
+        M02 = (int)dineroTotal/20;
+        dineroTotal = dineroTotal-M02*20;
+        M01 = (int)dineroTotal/10;
+        dineroTotal = dineroTotal-M01*10;
+        M005 = (int)dineroTotal/5;
+        dineroTotal = dineroTotal-M005*5;
+        M002 = (int)dineroTotal/2;
+        dineroTotal = dineroTotal-M002*2;
+        M001 = (int)dineroTotal;
+        System.out.println("Tienes "+B100+" billetes de 100€,"+B20+" billetes de 20€,"+B5+" billetes de 5€, "+M1+" monedas de 1€");
+        System.out.println("Tambien tienes "+M05+" monedas de 50 centimos,"+M02+" monedas de 20 centimos,"+M01+" monedas de 10 centimos,");
+        System.out.println(M005+" monedas de 5 centimos,"+M002+" monedas de 2 centimos y "+M001+" monedas de 1 centimo");
     }
 }
