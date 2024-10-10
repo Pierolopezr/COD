46d21d20e77bdb6b082013898f34255733b92cac
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-10-10 13:06:19 +0200

    Ejercicio 4 hecho

---
+++ BoletinExtra_2/src/main/java/org/example/Ej_4.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.util.Scanner;
+public class Ej_4 {
+    public static void main(String []args){
+        int B100,B20,B5,M1,dineroTotal;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cuantos dinero tienes?");
+        dineroTotal = sc.nextInt();
+        sc.close();
+        B100=(int)dineroTotal/100;
+        dineroTotal = dineroTotal-B100*100;
+        B20=(int)(dineroTotal/20);
+        dineroTotal = dineroTotal-B20*20;
+        B5 = (int)dineroTotal/5;
+        dineroTotal=dineroTotal-B5*5;
+        M1=dineroTotal;
+        System.out.println("Tienes "+B100+" billetes de 100€,"+B20+" billetes de 20€,"+B5+" billetes de 5€ y "+M1+" monedas de 1€");
+    }
+}
