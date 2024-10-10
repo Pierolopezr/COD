794cf17c2bd05d4daf6c0fa621deed3b3ad730ef
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-10-10 14:05:56 +0200

    Ejerccio 4 admitiendo decimales, solo te devuelve los cenitimos no sus monedas respectivas

--- BoletinExtra_2/src/main/java/org/example/Ej_4.java
+++ BoletinExtra_2/src/main/java/org/example/Ej_4.java
@@ -2,18 +2,22 @@ package org.example;
 import java.util.Scanner;
 public class Ej_4 {
     public static void main(String []args){
-        int B100,B20,B5,M1,dineroTotal;
+        int B100,B20,B5,M1;
+        double dineroTotal;
         Scanner sc = new Scanner(System.in);
         System.out.println("Cuantos dinero tienes?");
-        dineroTotal = sc.nextInt();
+        dineroTotal = sc.nextDouble();
         sc.close();
-        B100=dineroTotal/100;
+        System.out.println(dineroTotal);
+        B100=(int)(dineroTotal/100);
         dineroTotal = dineroTotal-B100*100;
-        B20=(dineroTotal/20);
+        B20=(int)(dineroTotal/20);
         dineroTotal = dineroTotal-B20*20;
-        B5 = dineroTotal/5;
+        B5 = (int)(dineroTotal/5);
         dineroTotal=dineroTotal-B5*5;
-        M1=dineroTotal;
-        System.out.println("Tienes "+B100+" billetes de 100€,"+B20+" billetes de 20€,"+B5+" billetes de 5€ y "+M1+" monedas de 1€");
+        M1=(int)dineroTotal;
+        dineroTotal = (dineroTotal-M1);
+        System.out.print("Tienes "+B100+" billetes de 100€,"+B20+" billetes de 20€,"+B5+" billetes de 5€, "+M1+" monedas de 1€ y ");
+        System.out.printf("%.2f centimos",dineroTotal);
     }
 }
