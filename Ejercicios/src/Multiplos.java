bc2c6b43e6992186c89bf4500377f4990745e095
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-10-23 09:21:03 +0200

    Ejercicio que te devuleve multiplos de 7 menores a 100 terminado sin if y con lógica

--- Ejercicios/src/Multiplos.java
+++ Ejercicios/src/Multiplos.java
@@ -1,11 +1,11 @@
 /**
  * Este código te devuelve los multiplos de 7 menores de 100
- * @version 1.1
+ * @version 1.2
  * @author Jorge Durán Cruz
  */
 public class Multiplos {
     public static void main(String[] args){
-        for (int i=0;i<96;){
+        for (int i=0;i<93;){
             i = i+7;
             System.out.println(i);
         }
