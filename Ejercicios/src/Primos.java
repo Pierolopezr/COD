96907880286282b0b95058439fb6b9830852744a
Piero López <plopezrosas@danielcastelao.org~>
2024-10-23 14:06:26 +0200

    numero n e inferiorees

--- Ejercicios/src/Primos.java
+++ Ejercicios/src/Primos.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Primos {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("¿Qué n úmero eliges?");
+        System.out.println("¿Qué n número eliges?");
         int n = sc.nextInt();
         sc.close();
         int divisores =0;
